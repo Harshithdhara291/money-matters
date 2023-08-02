d0ca0fdef33a0317b276ea19881b9a887d8742d1
Harshith <harshithdhara291@gmail.com>
2023-08-02 14:51:15 +0530

    updated react charts

--- src/components/ReactCharts/index.js
+++ src/components/ReactCharts/index.js
@@ -1,9 +1,8 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import React from 'react'
 import Cookies from 'js-cookie'
-import { useState,useEffect } from 'react';
-import LoadingView from '../Loading'
-import FailureView from '../FailureView'
+import { useEffect } from 'react';
+// import LoadingView from '../Loading'
+// import FailureView from '../FailureView'
 import {Bar} from 'react-chartjs-2'
 import {CategoryScale,Chart,LinearScale,BarElement,Legend,Title,Tooltip} from 'chart.js'; 
 import './index.css'
@@ -44,22 +43,21 @@ const options ={
     }
 }
 
-const apiStatusConstants = {
-  initial: 'INITIAL',
-  success: 'SUCCESS',
-  failure: 'FAILURE',
-  inProgress: 'IN_PROGRESS',
-}
+// const apiStatusConstants = {
+//   initial: 'INITIAL',
+//   success: 'SUCCESS',
+//   failure: 'FAILURE',
+//   inProgress: 'IN_PROGRESS',
+// }
 
 const ReactChart = () => {
-  const [apiStatus, setApiStatus] = useState(apiStatusConstants.initial)
     useEffect(() => {
         getLastWeekCreditDebit()
+        getLastWeekCreditDebitAdmin()
       }, []
     )
 
     const getLastWeekCreditDebit = async () => {
-        setApiStatus(apiStatusConstants.inProgress)
         const userId = Cookies.get('user_id')
         const apiUrl = `https://bursting-gelding-24.hasura.app/api/rest/daywise-totals-7-days`
         const options = {
@@ -75,13 +73,25 @@ const ReactChart = () => {
        
         if (response.ok) {
             const fetchedData = await response.json()
-        //   const updatedData = getFormattedData(fetchedData.users[0])
-          console.log(fetchedData)
-        //   console.log(updatedData)
-        //   setProfileData(updatedData)
-          setApiStatus(apiStatusConstants.success)
-        } else {
-          setApiStatus(apiStatusConstants.failure)
+          console.log(fetchedData,'react chart')
+        }
+      }
+
+      const getLastWeekCreditDebitAdmin = async () => {
+        const apiUrl = `https://bursting-gelding-24.hasura.app/api/rest/daywise-totals-last-7-days-admin`
+        const options = {
+        headers: {
+            'Content-Type' : 'application/json',
+            'x-hasura-admin-secret': 'g08A3qQy00y8yFDq3y6N1ZQnhOPOa4msdie5EtKS1hFStar01JzPKrtKEzYY2BtF',
+            'x-hasura-role' : 'admin',
+        },
+        method: 'GET',
+        }
+        const response = await fetch(apiUrl, options)
+       
+        if (response.ok) {
+            const fetchedData = await response.json()
+            console.log(fetchedData,'react chart admin')
         }
       }
 
@@ -95,23 +105,23 @@ const ReactChart = () => {
       }
 
     
-      const renderProfileData = () => {
-        switch (apiStatus) {
-          case apiStatusConstants.success:
-            return renderProfile()
-          case apiStatusConstants.failure:
-            return FailureView()
-          case apiStatusConstants.inProgress:
-            return LoadingView()
-          default:
-            return null
-        }
-      }
+      // const renderProfileData = () => {
+      //   switch (apiStatus) {
+      //     case apiStatusConstants.success:
+      //       return renderProfile()
+      //     case apiStatusConstants.failure:
+      //       return FailureView()
+      //     case apiStatusConstants.inProgress:
+      //       return LoadingView()
+      //     default:
+      //       return null
+      //   }
+      // }
 
   return (
     <div className='main-container-chart'>
         <h1 className='accounts-head-charts'>Debit & Credit Overview</h1>
-        {renderProfileData()}
+        {renderProfile()}
     </div>
   )
 }
